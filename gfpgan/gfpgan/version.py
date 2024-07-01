6141af040dd2691d4f0f362cae0267951b671250
Khang2411 <hk24112000@gmail.com>
2024-07-01 08:02:44 +0700

    gfpgan with django

---
+++ gfpgan/gfpgan/version.py
@@ -0,0 +1,5 @@
+# GENERATED VERSION FILE
+# TIME: Mon Jul  1 02:47:41 2024
+__version__ = '1.3.8'
+__gitsha__ = 'unknown'
+version_info = (1, 3, 8)
---
+++ gfpgan/views.py
@@ -0,0 +1,44 @@
+import os
+import subprocess
+from django.http import HttpResponse, JsonResponse
+import datetime
+from django.core.files.storage import FileSystemStorage
+from django.views.decorators.csrf import csrf_exempt
+import sys
+import shutil
+
+def home(request):
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+    return HttpResponse(html)
+
+
+@csrf_exempt
+def restore_photo(request):
+    myfile = request.FILES["image"]
+    fs = FileSystemStorage("gfpgan/inputs/upload")
+    filename = fs.save(myfile.name, myfile)
+    uploaded_file_url = fs.url(filename)
+    inference()
+    return JsonResponse({"img_src": uploaded_file_url})
+
+
+def run_cmd(command):
+    try:
+        print(command)
+        subprocess.call(command, shell=True)
+    except KeyboardInterrupt:
+        print("Process interrupted")
+        sys.exit(1)
+
+
+def inference():
+    INPUT_DIR = "inputs/upload"
+    OUTPUT_DIR = "results"
+    oldFolder = os.getcwd()
+    os.chdir('gfpgan')
+    shutil.rmtree(OUTPUT_DIR, ignore_errors=True)
+    run_cmd("python inference_gfpgan.py -s 2 -i " + INPUT_DIR + " -o " + OUTPUT_DIR)
+    shutil.rmtree(INPUT_DIR, ignore_errors=True)
+    os.chdir(oldFolder)
+    return os.path.join(OUTPUT_DIR, "1_out.jpg")
