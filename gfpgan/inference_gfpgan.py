6d638ecef0dc9bfbe1de41a7541f97b4db3cc29d
Khang2411 <hk24112000@gmail.com>
2024-07-03 21:53:46 +0700

    deploy 14

--- gfpgan/inference_gfpgan.py
+++ gfpgan/inference_gfpgan.py
@@ -56,37 +56,81 @@ def main():
 
     os.makedirs(args.output, exist_ok=True)
 
-    print('123')
-    print(img_list)
-    print('124')
-   
+    # ------------------------ set up background upsampler ------------------------
+    if args.bg_upsampler == 'realesrgan':
+        if not torch.cuda.is_available():  # CPU
+            import warnings
+            warnings.warn('The unoptimized RealESRGAN is slow on CPU. We do not use it. '
+                          'If you really want to use it, please modify the corresponding codes.')
+            bg_upsampler = None
+        else:
+            from basicsr.archs.rrdbnet_arch import RRDBNet
+            from realesrgan import RealESRGANer
+            model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=2)
+            bg_upsampler = RealESRGANer(
+                scale=2,
+                model_path='https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.1/RealESRGAN_x2plus.pth',
+                model=model,
+                tile=args.bg_tile,
+                tile_pad=10,
+                pre_pad=0,
+                half=True)  # need to set False in CPU mode
+    else:
+        bg_upsampler = None
+
     # ------------------------ set up GFPGAN restorer ------------------------
-    print('before loop loop')
-   
+    if args.version == '1':
+        arch = 'original'
+        channel_multiplier = 1
+        model_name = 'GFPGANv1'
+        url = 'https://github.com/TencentARC/GFPGAN/releases/download/v0.1.0/GFPGANv1.pth'
+    elif args.version == '1.2':
+        arch = 'clean'
+        channel_multiplier = 2
+        model_name = 'GFPGANCleanv1-NoCE-C2'
+        url = 'https://github.com/TencentARC/GFPGAN/releases/download/v0.2.0/GFPGANCleanv1-NoCE-C2.pth'
+    elif args.version == '1.3':
+        arch = 'clean'
+        channel_multiplier = 2
+        model_name = 'GFPGANv1.3'
+        url = 'https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.3.pth'
+    elif args.version == '1.4':
+        arch = 'clean'
+        channel_multiplier = 2
+        model_name = 'GFPGANv1.4'
+        url = 'https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.4.pth'
+    elif args.version == 'RestoreFormer':
+        arch = 'RestoreFormer'
+        channel_multiplier = 2
+        model_name = 'RestoreFormer'
+        url = 'https://github.com/TencentARC/GFPGAN/releases/download/v1.3.4/RestoreFormer.pth'
+    else:
+        raise ValueError(f'Wrong model version {args.version}.')
+
     # determine model paths
-    model_path = os.path.join('experiments/pretrained_models', 'GFPGANv1.3' + '.pth')
+    model_path = os.path.join('experiments/pretrained_models', model_name + '.pth')
     if not os.path.isfile(model_path):
-        model_path = os.path.join('gfpgan/weights', 'GFPGANv1.3' + '.pth')
+        model_path = os.path.join('gfpgan/weights', model_name + '.pth')
     if not os.path.isfile(model_path):
         # download pre-trained models from url
-        model_path = os.path.join('experiments/pretrained_models', 'GFPGANv1.3' + '.pth')
+        model_path = url
 
-    print('before restorer')
-    print(os.listdir('experiments/pretrained_models'))
-        
+    restorer = GFPGANer(
+        model_path=model_path,
+        upscale=args.upscale,
+        arch=arch,
+        channel_multiplier=channel_multiplier,
+        bg_upsampler=bg_upsampler)
+
     # ------------------------ restore ------------------------
-    print('before loop')
-    print(img_list)
     for img_path in img_list:
         # read image
-        print('img_path running')
         img_name = os.path.basename(img_path)
         print(f'Processing {img_name} ...')
         basename, ext = os.path.splitext(img_name)
         input_img = cv2.imread(img_path, cv2.IMREAD_COLOR)
 
         # restore faces and background if necessary
-        print('img_path 2 running')
         cropped_faces, restored_faces, restored_img = restorer.enhance(
             input_img,
             has_aligned=args.aligned,
@@ -110,7 +154,6 @@ def main():
             cmp_img = np.concatenate((cropped_face, restored_face), axis=1)
             imwrite(cmp_img, os.path.join(args.output, 'cmp', f'{basename}_{idx:02d}.png'))
 
-        print('img_path 3 running')
         # save restored img
         if restored_img is not None:
             if args.ext == 'auto':
